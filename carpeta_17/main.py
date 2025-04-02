97fbb4cd88592e9970ac5c79110181f8d46818b9
Mariana <marianapirachican94@gmail.com>
2025-04-02 08:35:30 -0500

    cambio

--- carpeta_17/main.py
+++ carpeta_17/main.py
@@ -1,4 +1,5 @@
 # Estudiante: corrige este código y haz un pull request con la versión corregida.
 
-texto = 'Hola Mundo'
-print(texto.remove('Mundo'))
+texto = "Hola Mundo"
+texto = texto.replace("Hola ", "")
+print(texto)
